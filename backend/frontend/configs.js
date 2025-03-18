61d03b64ef7ba272434e1b2d82996a5cdc4beafb
SasmithaDilshan <dilshanekanayaka669@gmail.com>
2025-03-18 14:23:09 +0530

    added front end

---
+++ backend/frontend/configs.js
@@ -0,0 +1,3 @@
+window.configs = {
+    apiUrl: 'https://eddb4fc5-5bf6-40a5-a54d-8ea2a3fcbaca-dev.e1-us-east-azure.choreoapis.dev/prism/gpt-researcher-backend/v1',
+};
---
+++ backend/frontend/nextjs/helpers/getHost.ts
@@ -0,0 +1,20 @@
+interface GetHostParams {
+  purpose?: string;
+}
+
+export const getHost = ({ purpose }: GetHostParams = {}): string => {
+  if (typeof window !== 'undefined') {
+    let { host } = window.location;
+    if (process.env.NEXT_PUBLIC_GPTR_API_URL) {
+      const apiUrl = process.env.CHOREO_GPT_RESEARCHER_BACKEND_SERVICEURL;
+      const apiKey = process.env.CHOREO_GPT_RESEARCHER_BACKEND_APIKEY;
+      return `${apiUrl}?api_key=${apiKey}`;
+      console.log('apiUrl', apiUrl);
+    } else if (purpose === 'langgraph-gui') {
+      return host.includes('localhost') ? 'http%3A%2F%2F127.0.0.1%3A8123' : `https://${host}`;
+    } else {
+      return host.includes('localhost') ? 'http://localhost:8000' : `https://${host}`;
+    }
+  }
+  return '';
+};
