e62088109de0f81fe73696dbf624701b06e9add1
SasmithaDilshan <dilshanekanayaka669@gmail.com>
2025-03-21 12:02:44 +0530

    configs

--- frontend/nextjs/helpers/getHost.ts
+++ frontend/nextjs/helpers/getHost.ts
@@ -19,18 +19,18 @@ interface HostResponse {
 export const getHost = async ({ purpose }: GetHostParams = {}): Promise<HostResponse> => {
   const getClientCredentials = clientCredentials(
     axios.create(),
-    process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_TOKENURL,
-    process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERKEY,
-    process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERSECRET
+    process.env.CHOREO_GPT_RESEARCHER_CONNECTION_TOKENURL,
+    process.env.CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERKEY,
+    process.env.CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERSECRET
   );
-  console.log(process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_SERVICEURL);
-  console.log(process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_TOKENURL);
-  console.log(process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERKEY);
-  console.log(process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERSECRET);
+  console.log(process.env.CHOREO_GPT_RESEARCHER_CONNECTION_SERVICEURL);
+  console.log(process.env.CHOREO_GPT_RESEARCHER_CONNECTION_TOKENURL);
+  console.log(process.env.CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERKEY);
+  console.log(process.env.CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERSECRET);
   const auth: AccessTokenResponse = await getClientCredentials('OPTIONAL_SCOPES');
   const accessToken: string = auth.access_token;
-  const acc = process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_TOKEN;
-  const ws = new WebSocket(`${process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_SERVICEURL}/ws`, ['choreo-oauth2-token',`${accessToken}`]);
+  const acc = process.env.CHOREO_GPT_RESEARCHER_CONNECTION_TOKEN;
+  const ws = new WebSocket(`${process.env.CHOREO_GPT_RESEARCHER_CONNECTION_SERVICEURL}/ws`, ['choreo-oauth2-token',`${accessToken}`]);
   const headers: string[] = [`sec-websocket-protocol: choreo-test-key, ${accessToken}`];
 
   return { ws, headers };
