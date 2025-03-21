7d55f59c3ea1ca1496f4705b67eeaae3a66117ee
SasmithaDilshan <dilshanekanayaka669@gmail.com>
2025-03-21 21:04:00 +0530

    configs

--- frontend/nextjs/helpers/getHost.ts
+++ frontend/nextjs/helpers/getHost.ts
@@ -16,21 +16,26 @@ interface HostResponse {
   headers: string[];
 }
 
+
 export const getHost = async ({ purpose }: GetHostParams = {}): Promise<HostResponse> => {
+  const config = await fetch('/config.json').then((response) => response.json());
+  console.log(config);
   const getClientCredentials = clientCredentials(
     axios.create(),
-    process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_TOKENURL,
-    process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERKEY,
-    process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERSECRET
+    config.CHOREO_GPT_RESEARCHER_CONNECTION_TOKENURL,
+    config.CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERKEY,
+    config.CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERSECRET
   );
-  console.log(process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_SERVICEURL);
-  console.log(process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_TOKENURL);
-  console.log(process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERKEY);
-  console.log(process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERSECRET);
+
+  console.log(config.CHOREO_GPT_RESEARCHER_CONNECTION_SERVICEURL);
+  console.log(config.CHOREO_GPT_RESEARCHER_CONNECTION_TOKENURL);
+  console.log(config.CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERKEY);
+  console.log(config.CHOREO_GPT_RESEARCHER_CONNECTION_CONSUMERSECRET);
+
   const auth: AccessTokenResponse = await getClientCredentials('OPTIONAL_SCOPES');
   const accessToken: string = auth.access_token;
-  const acc = process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_TOKEN;
-  const ws = new WebSocket(`${process.env.NEXT_PUBLIC_CHOREO_GPT_RESEARCHER_CONNECTION_SERVICEURL}/ws`, ['choreo-oauth2-token',`${accessToken}`]);
+  const acc = config.CHOREO_GPT_RESEARCHER_CONNECTION_TOKEN;
+  const ws = new WebSocket(`${config.CHOREO_GPT_RESEARCHER_CONNECTION_SERVICEURL}/ws`, ['choreo-oauth2-token', `${accessToken}`]);
   const headers: string[] = [`sec-websocket-protocol: choreo-test-key, ${accessToken}`];
 
   return { ws, headers };
